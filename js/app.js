e6df4b3979da106298f276dcdfe5348d06fb5309
Thomas Bullock laptop <motbollox@gmail.com>
2016-08-25 15:53:01 +1000

    refactored code. edited .posterbutton css

--- js/app.js
+++ js/app.js
@@ -1,76 +1,82 @@
-var MovieList = function() {
-	this.movies = [];
-}
+/// Data Object
 
-var film;
-var fuckyoulist = [];
-
-MovieList.prototype.add = function(movie) {
-	this.movies.push(movie);
-	// console.log(this.movies.length)
-}
-
-var favouriteMovieList = new MovieList();
-
-var MovieObject = function(pic, title, year, director, actors ,duration, genre, rating, plot, type, xFeature) {
-	this.pic = pic;
-	this.title = title;
-	this.year = year;
-	this.director = director;
-	this.actors = actors;
-	this.duration = duration;
-	this.genre = genre;
-	this.rating = rating;
-	this.plot = plot;
-	this.type = type;
-	this.xFeature = xFeature;
-	if (this.type != "Stinkers" && this.type != "search") {
-  		// Trailer.call(this, xFeature);		
-	} else {
-		// Review.call(this, xFeature);
-	};
-}
-
-var film = new MovieObject('http://img.omdbapi.com/?i=tt0387808&apikey=6c024fcd', 'Idoicracy', '2006', 'Mr Judge', 'McDood', '123 min', 'comdey', '6.6', 'fuck java', 'Comedy', 'https://www.youtube.com/embed/clYwX8Z43zg?autoplay=true&showinfo=0');
-// var favouriteMovieList = new MovieList(); 
-// favouriteMovieList.add(film);
-
-
-
-var data = {
+function data() {
 	motsFavs: null,
-	init: function() {
+	this.favPosters = [{title: "City of God", xpos: 0, ypos: 0}, {title: "Life of Pi", xpos: -101, ypos: 0},
+					 {title: "Taxi Driver", xpos: -201, ypos: 0}, {title: "Ghosts Of The Civil Dead", xpos: -301, ypos: 0},
+					 {title: "Dead Calm", xpos: -401, ypos: 0}, {title: "Gone Girl", xpos: -501, ypos: 0},
+					 {title: "Flirting", xpos: -601, ypos: 0}, {title: "Electroma", xpos: -701, ypos: 0},
+					 {title: "Withnail & I", xpos: -801, ypos: 0}, {title: "Dirty Pretty Things", xpos: -901, ypos: 0},
+					 {title: "Block Party", xpos: 0, ypos: 150}, {title: "Ex Machina", xpos: -101, ypos: 150},
+					 {title: "Nightcrawler", xpos: -201, ypos: 150}, {title: "Walk Hard", xpos: -301, ypos: 150},
+					 {title: "Apocalypse Now", xpos: -401, ypos: 150}, {title: "Precious", xpos: -501, ypos: 150},				 
+					 {title: "Revenge Of The Nerds", xpos: -601, ypos: 150}, {title: "Detachment", xpos: -701, ypos: 150},
+					 {title: "The Year My Voice Broke", xpos: -801, ypos: 150}, {title: "Sideways", xpos: -901, ypos: 150},
+					 {title: "Paris Texas", xpos: -801, ypos: 150}, {title: "History of the world part I", xpos: -901, ypos: 150}	 
+					]
+	this.init = function() {
 		var motsFavs;
-		// this.favouriteMovieList = new MovieList(); 
-		this.favCategory = [];
 		this.ajaxRequest(this.motsFavsData);
-	},
-	// MovieList: function() {
-	// 	this.movies = [];
-	// },
+	};
 
-	// addToMovieList: function(movie) {
-	// 	this.favouriteMovieList.movies.push(movie);			
-	// },
-
-
-	motsFavsData: function(returned_data) {
+	this.motsFavsData = function(returned_data) {
     	data.motsFavs = returned_data;
-    	// console.log(data.motsFavs)
 		data.genres = Object.keys(data.motsFavs)
-		// console.log(data.genres);
-		// data.genres.forEach(function(genre, index){
-		// 	var favourite = new this.Favourites(genre);
-		// 	data.favCategory.push(favourite)
-		// }, this)
-		// console.log(data.favCategory)
 		ui.init();
-	},
-	// populateFavs: function() {
+	};
 
-	// },
+	this.MovieObject = function(pic, title, year, director, actors ,duration, genre, rating, plot, type, xFeature) {
+		this.pic = pic;
+		this.title = title;
+		this.year = year;
+		this.director = director;
+		this.actors = actors;
+		this.duration = duration;
+		this.genre = genre;
+		this.rating = rating;
+		this.plot = plot;
+		this.type = type;
+		if (this.type != "Stinkers" && this.type != "search") {
+	  		data.Trailer.call(this, xFeature);		
+			this.xFeature = xFeature;
+		} else {
+			this.review = xFeature; // Review.call(this, xFeature);
+		};
+	};
 
-	ajaxRequest: function(callback) {
+	this.Trailer = function(trailer) {
+
+		this.trailer = trailer;
+		this.buttonID = controller.removeSp(this.title)+ 'Trailer';  /// this button ID maybe redundant now with changes to the mov
+		this.videoButton = {
+			buttonHTML: '<a href="#" id="' + this.buttonID + '" data-reveal-id="trailerModal" class="trailer button small radius success" href="#">Watch Trailer / Scene</a>',
+		};
+		this.modal = function(movie) {
+			if(this.trailer == "" || this.trailer == undefined)	{
+				console.log('denied')
+				return;
+			} else {
+				console.log(this)
+				ui.modal.innerHTML = "";
+				var htmlString = '<div class="">';
+				htmlString += '<h3>' + this.title + '</h3>';
+				htmlString += '<div class="flex-video">';
+				htmlString += '<iframe width="560" height="315" src="' + this.trailer + '" frameborder="0" allowfullscreen></iframe>';
+				htmlString += '</div>';
+				htmlString += '<a class="close-reveal-modal" aria-label="Close">&#215;</a>';
+				htmlString += '</div>';
+
+				ui.modal.innerHTML = htmlString;
+				$('.close-reveal-modal').click(function() {  //to stop video playing after modal closed wipe modal innerHTML
+					ui.modal.innerHTML = "";
+				})
+			}
+			}	
+	};	
+
+	this.prototype = Object.create(this.Trailer.prototype);
+
+	this.ajaxRequest = function(callback) {
 		var xmlhttp = new XMLHttpRequest();
 		var url = "../data/movieData.json";
 
@@ -82,147 +88,282 @@ var data = {
 		};
 		xmlhttp.open("GET", url, true);
 		xmlhttp.send();
-	},
+	};
 
-	movieOmdbRequest: function (movie, favGenre, xfeature) {
+	this.movieOmdbRequest = function (movie, favGenre, xfeature) {
+		var callback;
+		if (favGenre == "search") {
+			callback = createSearchResult;
+			console.log(favGenre + " search")
+		} else {
+			callback = createMovie;			
+		}
+
 		var omdbAPI = "http://www.omdbapi.com/?";
 			var omdbOptions = {
 				t: movie,
 				plot: "short",
 				r: "json"
 			};
-			function createMovie(returnData) {
-				console.log('success')
-				var omdbPoster = 'http://img.omdbapi.com/?i=' + returnData.imdbID + '&apikey=6c024fcd';
-				var favMovie = {};
-					favMovie.pic = omdbPoster;
-					favMovie.title = returnData.Title;
-					favMovie.year = returnData.Year;
-					favMovie.director = returnData.Director;
-					favMovie.actors = returnData.Actors;
-					favMovie.duration = returnData.Runtime;
-					favMovie.genre = returnData.Genre;
-					favMovie.rating = returnData.imdbRating;
-					favMovie.plot =  returnData.Plot;
-					favMovie.type = favGenre;
-					favMovie.xfeature = xfeature;
-				// console.log(favMovie);
-				favouriteMovieList.add(favMovie);
-				ui.renderMovieList(favouriteMovieList.movies, favGenre)
+			function createMovie(movData) {
+					var omdbPoster = 'http://img.omdbapi.com/?i=' + movData.imdbID + '&apikey=6c024fcd';
+					var favMovie = new data.MovieObject(omdbPoster, movData.Title, movData.Year, movData.Director, movData.Actors, movData.Runtime, movData.Genre, movData.imdbRating, movData.Plot, favGenre, xfeature)
+					ui.renderMovieCard(favMovie, favGenre)
+				}
+			
+			function createSearchResult(movData) {
+				if(movData.Error == "Movie not found!") {
+					$('#search').val("");
+					$('#search').attr("placeholder", "...Sorry Movie not found");
+				} else {
+					ui.updateStatus('search'); 
+					createMovie(movData); 
+				}
 			}
-			$.getJSON(omdbAPI, omdbOptions, createMovie);
-	},
-
-		//mots favs data^
-	// Favourites = function(genre) {
-
-	// 	this.name = genre;
-	// 	// console.log(genre);
-	// 	this.pic = this.motsFavs[genre].pic;
-	// 	this.favouritesList = this.motsFavs[genre].titles;
-	// }
-
-
-
-
-	// Favourites.prototype.toHTML = function() {
-	// 	htmlString = '<div class="wrapper">';
-	// 	htmlString += '<img src="' + this.pic + '">';
-	// 	htmlString += '<div class="cloak">';
-	// 	htmlString += '<h3>' + this.name + '</h3>';
-	// 	htmlString += '</div></div>';
-	// 	return htmlString;
-	// } 
+			$.getJSON(omdbAPI, omdbOptions, callback);
+	};
 	
 }; // end data object
 
-var controller = {
-		init: function() {
+// Controller Object
+function controller() {
+		this.init = function() {
 			data.init();
-		},
+		};
+
+	this.getFavGenre = function(index) {
+		var fav = data.genres[index]; 
+		return fav;
+	};
+	this.getMovieData = function() {
+		return data.motsFavs;
+	};
+
+	this.buildFavouriteList = function(genre) {
+		ui.updateStatus(genre);
+		var genreObj = data.motsFavs[genre];
+		if (genre != "Stinkers") {
+			for (var i = 0; i < genreObj.titles.length; i++) {
+			data.movieOmdbRequest(genreObj.titles[i], genre, genreObj.trailers[i]);  ///pass ajax function movie title, the genre list its from and a link to the trailer
+			}
+
+		} else {
+			for (var i = 0; i < genreObj.titles.length; i++) {
+				data.movieOmdbRequest(genreObj.titles[i], genre, genreObj.review[i]);  ///pass ajax function movie t
+			}	
+
+		}
+	};
+
+	this.search = function(search) {
+		console.log(search);
+		data.movieOmdbRequest(search, "search", null);			
+	}
+
+	this.getPosterData = function(index) {
+		return data.favPosters[index];
+	}
 
 //// HELPER FUNCTIONS /////
 
-//Shorten string - used to shorten genre data from OMDB
-shortenStr: function(str) {
-	for (var i = 0; i < str.length; i++) {
-		if (str.charAt(i) === ',') {
-			return str.substring(0, i);
+	//Shorten string - used to shorten genre data from OMDB
+	this.shortenStr = function(str) {
+		for (var i = 0; i < str.length; i++) {
+			if (str.charAt(i) === ',') {
+				return str.substring(0, i);
+			}
 		}
+		return str;
+	};
+
+	// remove spaces
+	this.removeSp = function(str) {
+		str = str.replace(/\s/g,'');  
+		return str;
+	};
+
+	// generate random number
+	this.randomNum = function(max) {
+		return Math.floor(Math.random() * max) + 1;
 	}
-	return str;
-}, 
 
-// remove spaces
-removeSp: function(str) {
-	str = str.replace(/\s/g,'');  
-	return str;
-},
-getFavGenre: function(index) {
-	// console.log(index);
-	// console.log(data.genres)
-	var fav = data.genres[index]; 
-	return fav;
-},
-getMovieData: function() {
-	return data.motsFavs;
-},
-
-
-buildFavouriteList: function(genre) {
-	// this.favouriteMovieList = [];
-	var genreObj = data.motsFavs[genre];
-	if (genre != "Stinkers") {
-		for (var i = 0; i < genreObj.titles.length; i++) {
-		data.movieOmdbRequest(genreObj.titles[i], genre, genreObj.trailers[i]);  ///pass ajax function movie title, the genre list its from and a link to the trailer
+	/// unique number generator
+	this.uniqueRandomNum = function(arr, tot, max) {
+		while(arr.length < tot){
+		  var r = controller.randomNum(max)-1
+		  var found = false;
+		  for(var i=0;i<arr.length;i++) {
+			if(arr[i]== r) {
+				found=true;break
+			}
+		  }
+		  if(!found)arr[arr.length] = r;
 		}
+	};
 
-	} else {
-		for (var i = 0; i < titlesList.length; i++) {
-			data.movieOmdbRequest(genreObj.titles[i], genre, genreObj.review[i]);  ///pass ajax function movie t
-		}	
+	}; // end controller object
 
-	}
-},
+/// User Interface Object
 
-
-// generate random number
-randomNum: function(max) {
-	return Math.floor(Math.random() * max) + 1;
-},
-
-/// unique number generator
-uniqueRandomNum: function(arr, tot, max) {
-	while(arr.length < tot){
-	  var r = controller.randomNum(max)-1
-	  var found = false;
-	  for(var i=0;i<arr.length;i++) {
-		if(arr[i]== r) {
-			found=true;break
-		}
-	  }
-	  if(!found)arr[arr.length] = r;
-	}
-}
-
-}; // end controller object
-
-
-var ui = {
-	init: function() {
+function ui() {
+	this.init = function() {
+		this.homerAnim = new this.Sprite (0, 0);
+		this.slide2 = document.getElementById('slide2')
+		this.slide3 = document.getElementById('slide3')	
+		this.creatPosterWall();
 		this.motsFavs = controller.getMovieData();
 		this.status = document.getElementById('status');
-		this.displayList = document.getElementById("displayList");
-		this.modal = document.getElementById("trailerModal");
+		this.displayList = document.getElementById('displayList');
+		this.modal = document.getElementById('trailerModal');
+		this.modalTitle = document.getElementById('modalTitle');
+		this.modalVideo = document.getElementById('modalVideo');
 		this.favButtonArray = [];
 		this.favButtonsRender();
+		$('.nav-link').click(function() {
+			var genre = $(this).text();
+			controller.buildFavouriteList(genre);
+		});
+		$('.stinkButton').click(function() {
+			var genre = $(this).text();
+			controller.buildFavouriteList(genre);
+		});
+		$('#go').click(function() {
+			var search = $('#search').val();
+			if (!search) { //ignore empty search field
+				return
+			} else {
+				controller.search(search);
+			}	
+		});
+	};
 
+	this.Sprite = function(x, y) {
+		this.index = 1;
+		this.count = 1;
+		this.x = x;
+		this.y = y;
+		this.imgWidth = 3900;
+		this.imgHeight = 300; // maybe not needed? 
+		this.xpos = 0;
+		this.ypos = 0;
+		this.numFrames = 10;
+		this.frameSize = 390;
+		this.reverse = false;
+		this.draw();
+	};
 
+	this.Sprite.prototype = {
+		draw: function() {
+            //multiplying by -1 because we want to move the image to the left and up to reveal the area we want to see.
+            var animHtml = '<div class="sprite"></div>';
+			this.animElement = $(animHtml);    
+			this.animElement.css({
+				"background-image": 'url("img/HomerSpriteSheet.jpg")', //' linear-gradient(90deg, rgba(170,87,69, 0) 0%,  #aa5745 35%)', 
+				backgroundPosition: (-this.xpos)+"px "+(-this.ypos)+"px",
+				width: this.frameSize,
+				height: this.imgHeight,
+				position: "absolute",
+				left: this.x,
+				top: this.y
 
-		// controller.uniqueRandomNum(favButtonArray, 4, 9)
-	},
+			});        	
+			$("#slide2").append(this.animElement);
+		}, 
+		animate: function() {
+           this.animElement.css({
+            	backgroundPosition: (-this.xpos)+"px "+(-this.ypos)+"px"
+            });  
+            //each time around we add the frame size to our xpos, moving along the source image.
+            this.xpos += this.frameSize;
+            //increase the index so we know which frame of our animation we are currently on.
+            this.index = this.index + this.count;
+            //if our index is higher than our total number of frames, we're at the end and better start over.
+	            if(this.index >= this.numFrames) {
+	            //	console.log("reversed");
+	            	this.reverse = true;
+	                this.count *= -1; 
+	                this.frameSize *= -1;
+			    } else if (this.index <= 1) {
+	            //	console.log("un-reversed");			    	
+			    	this.reverse = false;
+	                this.count *= -1; 
+	                this.frameSize *= -1;
+			    }
+		}
+	};
 
-	favButtonsRender: function() {
+	this.creatPosterWall = function() {
+		console.log(this);
+		this.posterWall = [];
+		var self = this;
+		var search = function() {
+			controller.search(this.title)
+		}
+		for (var i = 0; i < 20; i++) {
+			var poster = new Object()
+			poster.index = i;
+			poster.imgWidth = 1000;
+			poster.imgHeight = 300;
+			poster.nFrames = 20;
+
+			if (i < 10) {
+				poster.x = i * 100 
+				poster.y = 0				
+			} else {
+				poster.x = (i -10) * 100 
+				poster.y = 150					
+			}
+
+			poster.div = document.createElement('div')
+			var data = controller.getPosterData(poster.index);
+			$.extend(poster, data)
+			this.posterWall.push(poster);	
+		}
+		var arrLength = this.posterWall.length
+		for(var j = 0; j < 20; j++) {
+			var elem = this.posterWall[j].div;
+			elem.classList.add('posterButton');
+			// elem.dataset.title = this.posterWall[j].title;
+			elem.style.backgroundImage = 'url("img/PosterSpriteSheet.jpg")';
+			elem.style.backgroundPosition = this.posterWall[j].index * -100 -1 + "px " +  "0px";
+			// elem.style.border = "solid 1px #222"
+			elem.style.width = "100px";
+			elem.style.height = "150px"; 
+		 	elem.style.position = "absolute";
+     		elem.style.left = this.posterWall[j].x +"px";
+     		// console.log(this.posterWall[j].x)
+		    elem.style.top = this.posterWall[j].y +"px";
+		    
+		    elem.addEventListener('click', function(e) {
+		    	console.log(e.target)
+		    	controller.search(e.target.dataset.title)
+		    }, false)
+		    elem.innerHTML = '<div class="cloak" data-title="' + this.posterWall[j].title + '"></div>';
+		    this.slide3.appendChild(elem);
+		}
+	}
+
+	this.udpdateAnimations = function() {
+		// Slide 2 is active play animation
+		if (this.slide2.parentNode.classList.contains('active')) {
+			console.log('slide 2 active')
+			this.homerAnim.animate();
+		}
+
+	}
+
+	this.updateStatus = function(genre) {
+		if (genre == "Stinkers") {
+			this.status.innerHTML = '<h3>The Stinklist</h3>';	
+		} else if (genre ==  "search") {
+			this.status.innerHTML = '<h3>Search Results</h3>'
+		} else {
+			this.status.innerHTML = '<h3>Mots Favourite ' + genre + ' movies</h3>';
+		};
+		this.displayList.innerHTML = '';
+	};
+
+	this.favButtonsRender = function() {
 		controller.uniqueRandomNum(ui.favButtonArray, 4, 10)  // gets 4 random numbers
 
 		for (var i = 0; i < 4; i ++) {
@@ -247,59 +388,54 @@ var ui = {
 			}, true); 			
 
 		}
-	},
+	};
 
-	renderMovieList: function(list, genre) {
-		// this.movieList = favouriteMovieList;
-		// // var movieList = list;
-		// console.log(this.movieList.length);
-		// console.log(list.length);
-		// console.log(data.favouriteMovieList);
-		if (genre == "Stinkers") {
-			this.status.innerHTML = '<h3>The Stinklist</h3>';				
-		} else {
-			this.status.innerHTML = '<h3>Mots Favourite ' + genre + ' movies</h3>';
-		};
-		this.displayList.innerHTML = '';
-		console.log(list.length)	
-		list.forEach(function(movie, index, arr) {
-			// var movie = favouriteMovieList[i];
-			console.log(movie)
+	this.renderMovieCard = function(movie, genre) {
+		// console.dir(movie);
 			var htmlStr = '';
 			var movieEntry = document.createElement('div');
+
 			movieEntry.classList.add('movie', 'text-center', 'clearfix');
+
+
 			htmlStr += '<div><img class="pic" src="' + movie.pic + '" alt="' + movie.title +  '"></div>';
 			htmlStr += '<div><h3 class="title">' + movie.title + '</h3><span> (' + movie.year + ')  </span></div>';
 			htmlStr += '<span>' +  ' | ' + controller.shortenStr(movie.genre) + ' | ' + movie.duration + ' | </span>';
 			htmlStr += '<span class="rating"><img src="img/fi-star.svg"><b>' + movie.rating + '</b> /10 </span>';
-			htmlStr += '<ul>';
+			htmlStr += '<ul class="info-list">';
 			htmlStr += '<li class="entry"> Director: ' + movie.director + ' | Starring: ' + movie.actors + '</li>';
 			htmlStr += '<li class="entry">' + movie.plot + '</li>';
 			if(movie.type !== "Stinkers" && movie.type !== "search") {
-			// htmlStr += '<li class="entry">' + movie.videoButton.buttonHTML + '</li>';
+				htmlStr += '<li class="entry">' + movie.videoButton.buttonHTML + '</li>';
 			} else if (movie.type == "Stinkers" && movie.type != "search") {
-			htmlStr += '<li class="entry"><b>Mots thoughts: </b><i>' + movie.review + '</i></li>';
+				htmlStr += '<li class="entry"><b>Mots thoughts: </b><i>' + movie.review + '</i></li>';
 			}
 			htmlStr += '</ul>';	
-			console.log(htmlStr)
+			htmlStr += '</div>';
 			movieEntry.innerHTML = htmlStr;
+			if(movie.type !== "Stinkers" && movie.type !== "search") {
+				movieEntry.lastChild.lastChild.firstChild.addEventListener('click', function() {
+					movie.modal(movie)
+				})
+			}
+
 			displayList.appendChild(movieEntry);
-
-		}); 
-
-
-		}
-
+		};
 
 }; //end ui object
 
-	// Favourites.prototype.toHTML = function() {
-	// 	htmlString = '<div class="wrapper">';
-	// 	htmlString += '<img src="' + this.pic + '">';
-	// 	htmlString += '<div class="cloak">';
-	// 	htmlString += '<h3>' + this.name + '</h3>';
-	// 	htmlString += '</div></div>';
-	// 	return htmlString;
-	// } 
+// initiate application and timer
+var data = new data(),
+    ui = new ui(),
+    controller = new controller();
 
-controller.init(); //start app
+controller.init();
+
+setInterval(function() {
+
+		ui.udpdateAnimations();
+
+}, 140);
+
+
+
